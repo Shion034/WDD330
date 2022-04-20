a3e2e16a40c0f63bcf149781899b3f7a0acefdc4
Shion034 <skeaty431998@gmail.com>
2022-04-20 16:01:22 -0600

    up

--- js/main.js
+++ js/main.js
@@ -1,22 +1,36 @@
 
+let text ="";
 const lists = ["Week1 notes", "Week2 notes", "Week3 notes", "Week4 notes", "Week5 notes",
     "Week6 notes", "Week7 notes", "Week8 notes", "Week9 notes", "Week10 notes",
     "Week11 notes", "Week12 notes", "Week13 notes"];
+
+lists.forEach(myFunction);
 const links = [
-    { label: "Week1 notes", url: "week1/index.html" }
+    { label: "Week1 notes", url: "week1/index.html" },
+    { label: "Week2 notes", url: "week2/index.html" },
+    { label: "Week3 notes", url: "Week3/index.html" },
+    { label: "Week4 notes", url: "Week4/index.html" },
+    { label: "Week5 notes", url: "Week5/index.html" },
+    { label: "Week6 notes", url: "Week6/index.html" },
+    { label: "Week7 notes", url: "Week7/index.html" },
+    { label: "Week8 notes", url: "Week8/index.html" },
+    { label: "Week9 notes", url: "Week9/index.html" },
+    { label: "Week10 notes", url: "Week10/index.html" },
 ];
 
-function createElement() {
+document.getElementById("demo").innerHTML = text;
+function myFunction(item){
+    text += item + "<br>";
+}
+
+
+function addliElement() {
     const lielement = documnet.createElement("li");
-    const aelement = documnet.createElement("a");
-    let lioutput = document.getElementById("lioutput");
+    let lioutput = document.getElementById("lioutput").innerHTML = addliElement();
     lioutput.appendChild(lielement);
 }
 
-function createArea(){
-    var area = document.createElement("div");
-    area.innerHTML = "追加要素";
-    var demo = document.getElementById("demo");
-    demo.appendChild(area);
+function addlinkElement(){
+    const linkelement = documnet.createElement("a");
 
 }
