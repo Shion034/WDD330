240c573680b80093eef2567e48c4cba52e149b1a
Shion034 <skeaty431998@gmail.com>
2022-04-29 20:30:10 -0600

    week2 5

---
+++ Team activity/week 02/teamac2.js
@@ -0,0 +1,5 @@
+function getnum(){
+    let input = document.getElementById("_input").value
+    let input01 = parseFloat(input);
+    document.getElementById("_outpu").textContent = input01;
+}
--- week2/week2 ch4.js
+++ week2/week2 ch4.js
@@ -17,4 +17,41 @@ const goodbye = function bye() {
 //Invoking a function is to run the code inside the function’s body.To invoke a function, simply enter its name, followed by parentheses.
 
 //Return Values--------------------------------------
-//All functions return a value, which can be specified using the return statement, which comes after the return keyword. 
+//All functions return a value, which can be specified using the return statement, which comes after the return keyword.
+
+//Parameters and Arguments----------------------------
+function mean(a, b, c) {
+    return (a + b + c) / 3;
+}
+//Parameters and arguments are terms that are often used interchangeably to represent values provided for the function as an input.
+
+//Variable Numbers of Arguments-----------------------
+//Improved Mean Function--------------------------------
+function mean(...values) {
+    let total = 0;
+    for (const value of values) {
+        total += value;
+    }
+    return total / values.length;
+}
+
+//Default Parameters-------------------------------------
+function hello(name = 'World') {
+    console.log(`Hello ${name}!`);
+}
+
+//Arrow Functions----------------------------------------
+
+//Function Hoisting--------------------------------------
+//Variable Hoisting--------------------------------------
+//Callbacks----------------------------------------------
+function sing(song) {
+    console.log(`I'm singing along to ${song}`);
+}
+function sing(song, callback) {
+    console.log(`I'm singing along to ${song}.`);
+    callback();
+}
+
+//Sorting Arrays With A Callback--------------------------
+//Array Iterators-----------------------------------------
