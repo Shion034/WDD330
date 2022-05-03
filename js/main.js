08046d2225442123ad49a64033638a87a21fc86b
Shion034 <skeaty431998@gmail.com>
2022-05-03 17:42:38 -0600

    うｐ

--- js/main.js
+++ js/main.js
@@ -3,9 +3,9 @@
 const links = [
     { label: "Week1 notes", url: "week1/index1.html" },
     { label: "Week2 notes", url: "week2/index2.html" },
-    { label: "Week3 notes", url: "week2/index3.html" },
-    { label: "Week4 notes", url: "week2/index4.html" },
-    { label: "Week5 notes", url: "week2/index5.html" },
+    { label: "Week3 notes", url: "week3/index3.html" },
+    { label: "Week4 notes", url: "week4/index4.html" },
+    { label: "Week5 notes", url: "week5/index5.html" },
 ];
 //querySelectorを使ってHTML内の要素を自由に選択・取得することができる。
 const getOl = document.querySelector("ol");
