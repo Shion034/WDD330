70f1ec243afe2d85ab9204bb093ffcb38b6dc41e
Shion034 <skeaty431998@gmail.com>
2022-05-14 19:37:29 -0600

    up

---
+++ Team activity/week 04/main.js
@@ -0,0 +1,32 @@
+/*const box1 = document.getElementById("box1");
+box1.addEventListener('click', function(){
+    document.getElementById("box1").innerHTML="x";
+});*/
+const board = document.querySelector('main');
+const player1 = 'X';
+const player2 = 'O';
+let player = player1;
+let turn_counter = 0;
+const button = document.querySelector('button');
+
+function displayCharacter(box) {
+    box.target.innerHTML = player;
+    if (player === player1) {
+        player = player2;
+    } else {
+        player = player1;
+    }
+}
+
+function resetGame() {
+    console.log('Hello')
+    const board = document.querySelector('main');
+    for (let i = 0; i < board.children.length; i++) {
+        board.children[i].innerHTML = "";
+    }
+}
+
+board.addEventListener('click', displayCharacter);
+button.addEventListener('click', resetGame);
+
+
