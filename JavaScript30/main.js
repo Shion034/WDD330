8c07216197224300010c7506d0a59d5ee61d98a7
Shion034 <skeaty431998@gmail.com>
2022-06-08 15:27:55 -0600

    up

--- JavaScript30/main.js
+++ JavaScript30/main.js
@@ -1,9 +1,13 @@
 const links = [
     { label: "Day01", url: "Day01/day1.html"},
-    { label: "Day02", url: "Day02/day2.html" },
-    { label: "Day03", url: "Day03/day3.html" },
-    { label: "Day04", url: "Day04/day4.html" },
-    { label: "Day05", url: "Day05/day5.html" },
+    { label: "Day02", url: "Day02/index.html" },
+    { label: "Day03", url: "Day03/index.html" },
+    { label: "Day04", url: "Day04/index.html" },
+    { label: "Day05", url: "Day05/index.html" },
+    { label: "Day06", url: "Day06/index.html" },
+    { label: "Day07", url: "Day07/index.html" },
+    { label: "Day08", url: "Day08/index.html" },
+    { label: "Day09", url: "Day09/index.html" },
 ]
 
 const getol = document.querySelector("ol");
