3a412f3dddb002afa9b1f9a8e32b3365feb70d5f
Shion034 <skeaty431998@gmail.com>
2022-07-19 21:10:37 -0600

    up

--- Challange/main.js
+++ Challange/main.js
@@ -13,34 +13,109 @@ function ConvertCF(){
     console.log(celNum);
     console.log(TempretureA);
     OutputAreaT.innerHTML = TempretureA;
+    console.log("Now you selected Celsius");
     } else if (~sele.indexOf('Fahrenheit')){
-       TempretureA = Math.round((celNum-32)*5/9);
+        TempretureA = Math.round((celNum-32)*5/9);
         OutputAreaT.innerHTML = TempretureA;
+        console.log("Now you selected Fahrenheit");
     }}
 convert1.addEventListener('click', ConvertCF);
+
+/*------------------------------length------------------------------*/ 
+
+const convert2 = document.querySelector('.Convert2');
+
+function convertL() {
+    const celNum2 = document.querySelector('.numinput2').value;
+    const outputL = document.querySelector('.output2');
+    const sele2 = document.querySelector('.sele2').value;
+    const outPutsele2 = document.querySelector('.outputsele2').value;
+/********************************Kilimeter*************************************** */
+    if (~sele2.indexOf('Kilometer') && ~outPutsele2.indexOf('Yard')) {
+        LengthKtoY = celNum2 *1094;
+        outputL.innerHTML = LengthKtoY;
+        console.log("Now you selected Kilometer and Yard");
+    } else if (~sele2.indexOf('Kilometer') && ~outPutsele2.indexOf('Foot')) {
+        LengthKtoF = celNum2 * 3281;
+        outputL.innerHTML = LengthKtoF;
+        console.log("Now you selected Kilometer and Foot");
+    } else if (~sele2.indexOf('Kilometer') && ~outPutsele2.indexOf('Inch')) {
+        LengthKtoI = celNum2 * 39370;
+        outputL.innerHTML = LengthKtoI; 
+        console.log("Now you selected Kilometer and Inch");
+    }
+/********************************Meter*************************************** */    
+    else if (~sele2.indexOf('Meter') && ~outPutsele2.indexOf('Yard')) {
+        LengthMtoY = celNum2 * 1.094;
+        outputL.innerHTML = LengthMtoY;
+        console.log("Now you selected Meter and Yard");
+    } else if (~sele2.indexOf('Meter') && ~outPutsele2.indexOf('Foot')) {
+        LengthMtoF = celNum2 * 3.281;
+        outputL.innerHTML = LengthMtoF;
+        console.log("Now you selected Meter and Foot");
+    } else if (~sele2.indexOf('Meter') && ~outPutsele2.indexOf('Inch')) {
+        LengthMtoI = celNum2 * 39.37;
+        outputL.innerHTML = LengthMtoI; 
+        console.log("Now you selected Meter and Inch");
+    }
+/********************************Centimeter*************************************** */
+    else if (~sele2.indexOf('Centimeter') && ~outPutsele2.indexOf('Yard')) {
+        LengthCtoY = celNum2 /91.44;
+        outputL.innerHTML = LengthCtoY; 
+        console.log("Now you selected Centimeter and Yard");
+    } else if (~sele2.indexOf('Centimeter') && ~outPutsele2.indexOf('Foot')) {
+        LengthCtoF = celNum2 / 30.48;
+        outputL.innerHTML = LengthCtoF; 
+        console.log("Now you selected Centimeter and Foot");
+    } else if (~sele2.indexOf('Centimeter') && ~outPutsele2.indexOf('Inch')) {
+        LengthCtoI = celNum2 / 2.54;
+        outputL.innerHTML = LengthCtoI; 
+        console.log("Now you selected Centimeter and Inch");
+    }
+/********************************Millimeter*************************************** */
+    else if (~sele2.indexOf('Millimeter') && ~outPutsele2.indexOf('Yard')) {
+        LengthMmtoY = celNum2 / 914.4;
+        outputL.innerHTML = LengthMmtoY; 
+        console.log("Now you selected Millimeter and Yard");
+    } else if (~sele2.indexOf('Millimeter') && ~outPutsele2.indexOf('Foot')) {
+        LengthMmtoF = celNum2 / 304.8;
+        outputL.innerHTML = LengthMmtoF;
+        console.log("Now you selected Millimeter and Foot");
+    } else if (~sele2.indexOf('Millimeter') && ~outPutsele2.indexOf('Inch')) {
+        LengthMmtoI = celNum2 / 25.4;
+        outputL.innerHTML = LengthMmtoI; 
+        console.log("Now you selected Millimeter and Inch");
+    }    
+}
+
+convert2.addEventListener('click', convertL);
 
 /*------------------------------Weight------------------------------*/ 
 const convert3 = document.querySelector('.Convert3');
 
 function convertW(){
-const ConverNum = document.querySelector('.numinput3').value;
+const celNum3 = document.querySelector('.numinput3').value;
 const outputW = document.querySelector('.output3');
 const sele3 = document.querySelector('.sele3').value;
 const outPutsele3 = document.querySelector('.outputsele3').value;
 
-    if (~sele3.indexOf('gram') && ~outPutsele3.indexOf('ounce')) {
-        weightGtoO = ConverNum/28.35;
+    if (~sele3.indexOf('Gram') && ~outPutsele3.indexOf('Ounce')) {
+        weightGtoO = celNum3 / 28.35;
         outputW.innerHTML = weightGtoO;
-        console.log('hello3');
-    } else if (~sele3.indexOf('gram') && ~outPutsele3.indexOf('pound')) {
-        weightGtop = ConverNum/453.6;
+        console.log("Now you selected Gram and Ounce");
+    } else if (~sele3.indexOf('Gram') && ~outPutsele3.indexOf('Pound')) {
+        weightGtop = celNum3 / 453.6;
         outputW.innerHTML = weightGtop;
-    } else if (~sele3.indexOf('kilogram') && ~outPutsele3.indexOf('ounce') ) {
-        weightKtoO = Math.round(ConverNum*35.274);
+        console.log("Now you selected Gram and Pound");
+    } else if (~sele3.indexOf('Kilogram') && ~outPutsele3.indexOf('Ounce')) {
+        weightKtoO = Math.round(celNum3 * 35.274);
         outputW.innerHTML = weightKtoO;
-}
-
-
+        console.log("Now you selected Kilogram and Ounce");
+    } else if (~sele3.indexOf('Kilogram') && ~outPutsele3.indexOf('Pound')) {
+        weightKtoO = Math.round(celNum3 * 2.205);
+        outputW.innerHTML = weightKtoO;
+        console.log("Now you selected Kilogram and Pound");
+    }
 }
 
 convert3.addEventListener('click',convertW);
